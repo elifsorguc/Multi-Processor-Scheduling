596f8ac5d02596814424ad1d671b144c8e5e741e
Elif Sorguç <77637770+elifsorguc@users.noreply.github.com>
2024-11-12 21:38:59 +0300

    cleanup

--- mschedule/src-old/thread_manager.h
+++
@@ -1,8 +0,0 @@
-#ifndef THREAD_MANAGER_H
-#define THREAD_MANAGER_H
-
-// Function prototypes for thread management
-void create_processor_threads(int num_processors, scheduling_algorithm algo);
-void wait_for_threads_to_finish();
-
-#endif
